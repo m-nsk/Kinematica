da3d2bcae47793faa8ffa3b5dce4022161ec0570
Minjae Kung <minsk@unc.edu>
2022-12-20 18:23:00 -0500

    structured script file

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+// Classes
+class InelasticCollision {
+    constructor(m1, m2, v1i, v2i) {
+        this._m1 = m1;
+        this._m2 = m2;
+        this._v1i = v1i;
+        this._v2i = v2i;
+    }
+
+    calculate() {
+    }
+
+}
+
 // Dark mode toggle
 document
     .querySelector("#dark-mode-toggle")
     .addEventListener('click', () => {
         document.body.classList.toggle("latex-dark");
     });
+// Inelastic Collision m1
+document
+    .querySelector("i-m1")
+    .addEventListener('input', (event) => {
 
-// Date
-// n = new Date();
-// y = n.getFullYear();
-// m = n.getMonth() + 1;
-// d = n.getDate();
-// document.getElementById("date").innerHTML = y + "-" + m + "-" + d;
+    });
+/*
+Date
+n = new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = y + "-" + m + "-" + d;
+*/
