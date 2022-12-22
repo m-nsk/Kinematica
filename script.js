ef0a66e8489b7da42c9bdef479118e3a3a2ee4ef
Minjae Kung <minsk@unc.edu>
2022-12-22 01:22:00 -0500

    event listener

--- script.js
+++ script.js
@@ -21,23 +21,32 @@ class InelasticCollision {
         return this._v2i;
     }
 
-    // Methods
+    // find empty box
     findEmpty() {
-        if (this.m1 == null) {
+        if (this.m1 == 0) {
             return "m1";
-        } else if (this.m2 == null) {
+        } else if (this.m2 == 0) {
             return "m2";
-        } else if (this.v1i == null) {
+        } else if (this.v1i == 0) {
             return "v1i";
-        } else if (this.v2i == null) {
+        } else if (this.v2i == 0) {
             return "v2i";
-        } else if (this.vf == null) {
+        } else if (this.vf == 0) {
             return "vf";
         } else {
             return "none";
         }
     }
-    // calculate() {
+
+    calculate(empty) {
+        if (empty == "m1") {
+            // m1v1i + m2v2i = m1vf + m2vf
+            // m1v1i - m1vf = m2vf - m2v2i
+            // m1 = m2(vf - v2i) / (v1i - vf)
+            this.m1 = this.m2 * (this.vf - this.v2i) / (this.v1i - this.vf);
+            document.getElementById("i-m1").value = this.m1;
+        }
+    }
 
 }
 /* (this.m1 == null && this.m2 != null && this.v1i != null && this.v2i != null && this.vf != null) ||
@@ -52,12 +61,37 @@ document
     .addEventListener('click', () => {
         document.body.classList.toggle("latex-dark");
     });
-// Inelastic Collision m1
-document
-    .querySelector("i-m1")
-    .addEventListener('input', (event) => {
 
+// Inelastic Collision
+document
+    .querySelector("#calculate-ic")
+    .addEventListener('click', () => {
+        // // set current variables
+        // const inelasticCollision = new InelasticCollision(
+        //     document.getElementById("i-m1").value,
+        //     document.getElementById("i-m2").value,
+        //     document.getElementById("i-v1i").value,
+        //     document.getElementById("i-v2i").value,
+        //     document.getElementById("i-vf").value
+        // );
+        // // calculate with current variables
+        // inelasticCollision.calculate(inelasticCollision.findEmpty());
+        alert("Bruh")
     });
+
+
+function calculateIC()  {
+    let inelasticCollision = new InelasticCollision(
+        document.getElementById("i-m1").value,
+        document.getElementById("i-m2").value,
+        document.getElementById("i-v1i").value,
+        document.getElementById("i-v2i").value,
+        document.getElementById("i-vf").value
+    );
+    // calculate with current variables
+    inelasticCollision.calculate(inelasticCollision.findEmpty());
+    console.log("Bruh")
+}
 /*
 Date
 n = new Date();
