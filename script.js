22791eaf66904fbdea8fed97d833aebdd1b77286
Minjae Kung <minsk@unc.edu>
2022-12-22 15:27:03 -0500

    reorganized function calls

--- script.js
+++ script.js
@@ -23,37 +23,68 @@ class InelasticCollision {
 
     // find empty box
     findEmpty() {
-        if (this.m1 == 0) {
-            return "m1";
-        } else if (this.m2 == 0) {
-            return "m2";
-        } else if (this.v1i == 0) {
-            return "v1i";
-        } else if (this.v2i == 0) {
-            return "v2i";
-        } else if (this.vf == 0) {
-            return "vf";
-        } else {
-            return "none";
+        // find empty box; return alert if more than one empty box
+        let empty = [];
+        if (this.m1 == "") {
+            empty.push("m1");
         }
+        if (this.m2 == "") {
+            empty.push("m2");
+        }
+        if (this.v1i == "") {
+            empty.push("v1i");
+        }
+        if (this.v2i == "") {
+            empty.push("v2i");
+        }
+        if (this.vf == "") {
+            empty.push("vf");
+        }
+        if (empty.length == 0) {
+            alert("Please leave one box empty.");
+            return false;
+        }
+        if (empty.length > 1) {
+            return "error"
+        }
+        return empty[0];
+
     }
 
-    calculate(empty) {
+    calculate() {
+        const empty = this.findEmpty();
         if (empty == "m1") {
             // m1v1i + m2v2i = m1vf + m2vf
             // m1v1i - m1vf = m2vf - m2v2i
             // m1 = m2(vf - v2i) / (v1i - vf)
             this.m1 = this.m2 * (this.vf - this.v2i) / (this.v1i - this.vf);
             document.getElementById("i-m1").value = this.m1;
+        } else if (empty == "m2") {
+            
+        } else if (empty == "v1i") {
+
+        } else if (empty == "v2i") {
+
+        } else if (empty == "vf") {
+
+        } else {
+            alert("Please leave one box empty.");
         }
     }
 
 }
-/* (this.m1 == null && this.m2 != null && this.v1i != null && this.v2i != null && this.vf != null) ||
-(this.m1 != null && this.m2 == null && this.v1i != null && this.v2i != null && this.vf != null) ||
-(this.m1 != null && this.m2 != null && this.v1i == null && this.v2i != null && this.vf != null) ||
-(this.m1 != null && this.m2 != null && this.v1i != null && this.v2i == null && this.vf != null) ||
-(this.m1 != null && this.m2 != null && this.v1i != null && this.v2i != null && this.vf == null)*/
+
+function calculateIC() {
+    // set current variables
+    let inelasticCollision = new InelasticCollision(
+        document.getElementById("i-m1").value,
+        document.getElementById("i-m2").value,
+        document.getElementById("i-v1i").value,
+        document.getElementById("i-v2i").value,
+        document.getElementById("i-vf").value
+    );
+    inelasticCollision.calculate();
+}
 
 // Dark mode toggle
 document
@@ -65,33 +96,8 @@ document
 // Inelastic Collision
 document
     .querySelector("#calculate-ic")
-    .addEventListener('click', () => {
-        // // set current variables
-        // const inelasticCollision = new InelasticCollision(
-        //     document.getElementById("i-m1").value,
-        //     document.getElementById("i-m2").value,
-        //     document.getElementById("i-v1i").value,
-        //     document.getElementById("i-v2i").value,
-        //     document.getElementById("i-vf").value
-        // );
-        // // calculate with current variables
-        // inelasticCollision.calculate(inelasticCollision.findEmpty());
-        alert("Bruh")
-    });
+    .addEventListener('click', calculateIC);
 
-
-function calculateIC()  {
-    let inelasticCollision = new InelasticCollision(
-        document.getElementById("i-m1").value,
-        document.getElementById("i-m2").value,
-        document.getElementById("i-v1i").value,
-        document.getElementById("i-v2i").value,
-        document.getElementById("i-vf").value
-    );
-    // calculate with current variables
-    inelasticCollision.calculate(inelasticCollision.findEmpty());
-    console.log("Bruh")
-}
 /*
 Date
 n = new Date();
